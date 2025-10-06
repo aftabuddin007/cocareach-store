6e5b78d86fb93b6c1196868b99cac3f71d644608
Md Aftab Uddin <aftabhasan7856@gmail.com>
2025-10-07 00:31:16 +0600

    Add search option

--- src/Pages/Products/Products.jsx
+++ src/Pages/Products/Products.jsx
@@ -1,15 +1,19 @@
-import React from 'react';
+import React, { useState } from 'react';
 import useProducts from '../../Hooks/useProducts';
 import ProductCard from '../../Components/Product/ProductCard';
 
 const Products = () => {
+    const [search,setSearch]=useState('')
     const {products}=useProducts()
 
     return (
         <div>
             <div className='flex justify-between items-center py-4'>
-                <h2 className='font-bold text-2xl'>All Products</h2>
-                <button to='/products' className='btn font-semibold text-[17px]  bg-blue-300 hover:bg-blue-100'>Search</button>
+                <h2 className='font-bold text-2xl'>All Products <span className='text-sm text-gray-500'>({products.length}) product found</span></h2>
+                <label className="input">
+  
+  <input onChange={()=>setSearch()} type="search" placeholder="Search Products" />
+</label>
             </div>
            <div className='grid grid-cols-3 gap-4'>
              {
